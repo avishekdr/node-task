05dbb06e234221aba6b7982583c12f151200ee82
Avishek Datta Ray <avishek.ray@programmers.io>
2022-06-27 17:18:11 +0530

    database setup implemented

---
+++ src/models/post.js
@@ -0,0 +1,13 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Post extends Model {};
+    Post.init({
+        id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+        name: { type: DataTypes.STRING(60), allowNull: false, defaultValue: '' },
+        cost: { type: DataTypes.DECIMAL(10, 2), allowNull: false, defaultValue: 0 },
+        description: { type: DataTypes.TEXT, allowNull: false, defaultValue: '' }
+    }, { sequelize, modelName: 'Post', tableName: 'post', timestamps: true });
+    return Post;
+};

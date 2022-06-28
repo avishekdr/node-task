3c468314ce5b7331bae601e515b3141046f5d09b
Avishek Datta Ray <avishek.ray@programmers.io>
2022-06-28 10:50:02 +0530

    swagger setup

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 const express = require('express'), bodyParser = require('body-parser'),
-    app = express(), db = require('./models');
+    app = express(), db = require('./models'), swaggerUI = require('swagger-ui-express'),
+    YAML = require('yamljs'), swaggerJsDoc = YAML.load('src\/doc.yaml');
 
 db.sequelize.sync().then(() => console.log('Drop and re-sync db'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/post', require('./controller/post.controller'));
+app.use('/swagger-doc', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 
 // Exporting the app to server.js
 module.exports = app;

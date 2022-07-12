25c9caba5c89290a61f2e44c7bd6bf0cb14d8fc3
Avishek Datta Ray <avishek.ray@programmers.io>
2022-07-12 18:10:00 +0530

    cors origin logic added

--- src/api.js
+++ src/api.js
@@ -5,6 +5,14 @@ const express = require('express'), bodyParser = require('body-parser'),
 db.sequelize.sync().then(() => console.log('Drop and re-sync db'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// Api for CORS to connect Angular with backend
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/post', require('./controller/post.controller'));
 app.use('/swagger-doc', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 
